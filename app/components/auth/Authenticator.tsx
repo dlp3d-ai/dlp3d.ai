140bb605c66b06a1d622ac9a3a61965c4defcbd4
tong <360580347@qq.com>
2025-11-18 11:21:30 +0800

    Update app/components/auth/Authenticator.tsx
    Co-authored-by: teemo <35056621+WYK96@users.noreply.github.com>

--- app/components/auth/Authenticator.tsx
+++ app/components/auth/Authenticator.tsx
@@ -312,10 +312,6 @@ export default function Authenticator({ onAuthSuccess }: AuthenticatorProps) {
       return
     }
     setNeedCode(true)
-    //   setNeedCoolDown(true)
-    // } else {
-    //   showErrorNotification(res.auth_msg)
-    // }
   }
 
   const buttonText = (() => {
